e20aa7d760fa7ff93291fbe74a54dc8e2df024df
Valera <valera.kaz2017@yandex.ru>
2025-04-18 16:14:58 +0300

    Update config for production

--- svelte.config.js
+++ svelte.config.js
@@ -1,23 +1,23 @@
-import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import { mdsvex } from 'mdsvex';
+import adapter from '@sveltejs/adapter-static'; // Изменено с adapter-auto
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+  preprocess: [vitePreprocess(), mdsvex()],
+  
   kit: {
     adapter: adapter({
-      fallback: '404.html' // Для SPA-роутинга
+      pages: 'build',
+      assets: 'build',
+      fallback: '404.html' // Важно для SPA-роутинга
     }),
-    prerender: {
-      handleHttpError: ({ path, referrer, message }) => {
-        // Игнорируем ошибки, связанные с отсутствием БД
-        if (path.startsWith('/api') || message.includes('DATABASE_URL')) {
-          return;
-        }
-        throw new Error(message);
-      }
+    paths: {
+      base: process.argv.includes('dev') ? '' : process.env.BASE_PATH // Для GitHub Pages
     }
   },
-  preprocess: vitePreprocess()
+
+  extensions: ['.svelte', '.svx']
 };
 
 export default config;
