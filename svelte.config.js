c3cd9656132cc76c99993c661b2deeed46faf5ba
Valera <valera.kaz2017@yandex.ru>
2025-04-18 15:49:09 +0300

    Add GitHub Pages deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,23 @@
 import { mdsvex } from 'mdsvex';
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static'; // Изменено с adapter-auto
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: [vitePreprocess(), mdsvex()],
-	kit: { adapter: adapter() },
-	extensions: ['.svelte', '.svx']
+  preprocess: [vitePreprocess(), mdsvex()],
+  
+  kit: {
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: '404.html' // Важно для SPA-роутинга
+    }),
+    paths: {
+      base: process.argv.includes('dev') ? '' : process.env.BASE_PATH // Для GitHub Pages
+    }
+  },
+
+  extensions: ['.svelte', '.svx']
 };
 
 export default config;
