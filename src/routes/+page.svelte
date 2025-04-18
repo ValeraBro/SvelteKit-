4acfc39c4a843c862f89db06fd722e8d414a0e8f
Valera <valera.kaz2017@yandex.ru>
2025-04-18 15:24:30 +0300

    Fixed gitignore

---
+++ src/routes/+page.svelte
@@ -0,0 +1,27 @@
+<script>
+    import { theme } from '$lib/stores/theme';
+    import { onMount } from 'svelte';
+    
+    let currentTheme = 'light';
+    
+    onMount(() => {
+        const unsubscribe = theme.subscribe($theme => {
+            currentTheme = $theme;
+        });
+        
+        return () => unsubscribe();
+    });
+</script>
+
+<h1>Добро пожаловать в мое приложение!</h1>
+<p>Текущая тема: {currentTheme === 'dark' ? 'Тёмная' : 'Светлая'}</p>
+
+<style>
+    h1 {
+        margin-top: 60px;
+        text-align: center;
+    }
+    p {
+        text-align: center;
+    }
+</style>
